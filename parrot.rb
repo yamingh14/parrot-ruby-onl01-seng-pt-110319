1e52bdc865fa1bc096586cbdf40c25454cba731b
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-02-11 02:40:46 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,35 @@
+# def parrot(name = "Squawk!")
+#   puts "Pretty bird!"
+#   puts "Squawk!"
+#   "Squawk!"
+# end
+
 def parrot(sound = "Squawk!")
-puts sound
-return sound
+  puts sound
+  sound
 end
-parrot
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def parrot(sound = "Squawk!")
+# puts sound
+# return sound
+# end
+# parrot
   # Create method `parrot` that outputs a given phrase and
 # returns the phrase
